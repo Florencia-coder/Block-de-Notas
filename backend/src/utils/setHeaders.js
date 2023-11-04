49547a69e75d395849cc075684ab23a4274bfe13
FlorenciaFlandes <florenciaflandes@gmail.com>
2023-11-04 16:04:50 -0300

    set header with url prod

--- backend/src/utils/setHeaders.js
+++ backend/src/utils/setHeaders.js
@@ -1,6 +1,9 @@
 function setHeaders(req, res, next) {
   //Aca estamos seteando todos nuestros headers.
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://block-de-notas-frontend.vercel.app/"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
