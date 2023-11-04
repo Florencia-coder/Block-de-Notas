ec5a23a5063a919d19864e5fc3d68c9159ab523d
FlorenciaFlandes <florenciaflandes@gmail.com>
2023-11-04 14:43:24 -0300

    last config

--- backend/index.js
+++ backend/index.js
@@ -2,39 +2,39 @@ import express from "express";
 import sequelize from "./src/db.js";
 import "./src/models/Note.js";
 import "./src/models/Category.js";
+import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3000;
-const app = express();
 
-// async function main() {
-//   try {
-//     await sequelize.authenticate();
-//     console.log("Connection has been established successfully.");
-//     await sequelize.sync({ force: true });
-//     console.log("Synchronize tables with database successfully");
-//     app.listen(PORT, () => {
-//       console.log(`Escuchando en el puerto ${PORT}`); // eslint-disable-line no-console
-//     });
-//   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-//   }
-// }
-// main();
+async function main() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    await sequelize.sync({ force: true });
+    console.log("Synchronize tables with database successfully");
+    app.listen(PORT, () => {
+      console.log(`Escuchando en el puerto ${PORT}`); // eslint-disable-line no-console
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+main();
 
-app.get("/", (req, res) => {
-  const htmlResponse = `
-  <html>
-  <head>
-  <title>Express</title>
-  </head>
-  <body>
-  <h1>Soy un proyecto de back en vercel</h1>
-  </body>
-  </html>
-  `;
-  res.send(htmlResponse);
-});
+// app.get("/", (req, res) => {
+//   const htmlResponse = `
+//   <html>
+//   <head>
+//   <title>Express</title>
+//   </head>
+//   <body>
+//   <h1>Soy un proyecto de back en vercel</h1>
+//   </body>
+//   </html>
+//   `;
+//   res.send(htmlResponse);
+// });
 
-app.listen(PORT, () => {
-  console.log(`Escuchando en el puerto ${PORT}`); // eslint-disable-line no-console
-});
+// app.listen(PORT, () => {
+//   console.log(`Escuchando en el puerto ${PORT}`); // eslint-disable-line no-console
+// });
