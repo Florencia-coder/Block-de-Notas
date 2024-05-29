2a206514d629aa1d0cce15bf898a57e8aafa1f8a
Florencia Flandes <86976757+Florencia2022@users.noreply.github.com>
2024-05-29 12:56:21 -0300

    Update setHeaders.js

--- backend/src/utils/setHeaders.js
+++ backend/src/utils/setHeaders.js
@@ -1,6 +1,6 @@
 function setHeaders(req, res, next) {
   //Aca estamos seteando todos nuestros headers.
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://block-de-notas-frontend-eic2ftgfr-florencias-projects-f288cb3c.vercel.app/");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
