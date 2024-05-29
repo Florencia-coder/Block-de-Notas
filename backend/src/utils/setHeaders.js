926b46f1bb6d85ffbcb2e587a8d2246cedaf285c
Florencia <florenciaflandes@gmail.com>
2024-05-29 15:15:50 -0300

    change file setHeaders

--- backend/src/utils/setHeaders.js
+++ backend/src/utils/setHeaders.js
@@ -1,6 +1,6 @@
 function setHeaders(req, res, next) {
   //Aca estamos seteando todos nuestros headers.
-  res.header("Access-Control-Allow-Origin", "https://block-de-notas-frontend-eic2ftgfr-florencias-projects-f288cb3c.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://block-de-notas-frontend-9v9ceern6-florencias-projects-f288cb3c.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
